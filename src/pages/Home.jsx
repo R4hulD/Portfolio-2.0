88233fc2b7df01d1e32ff28a490a329eca402d87
Dhawan <RDHAWAN7@ford.com>
2025-08-14 21:00:21 +0530

    first commit, added Home page, Navbar and Background Components as well as css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { Navbar } from "../components/Navbar";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+
+export const Home = () => {
+    return <div className="min-h-dvh bg-background text-foreground overflow-x-hidden">
+
+        {/* Theme Toggle */ }
+        <ThemeToggle />
+        {/* Background Effects */ }
+        <StarBackground />
+        {/* Navbar */ }
+        <Navbar />
+        {/* Main Content */ }
+        <main>
+            <HeroSection />
+            <AboutSection />
+        </main>
+        {/* Footer */ }
+    </div>;
+};
